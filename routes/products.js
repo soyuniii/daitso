d6e6df55c97e16b65f1ca0132f7c2f02f8040ce0
soyuniii <pe1229@naver.com>
2025-06-03 20:22:56 +0900

    상세페이지 구현

--- routes/products.js
+++ routes/products.js
@@ -2,16 +2,32 @@
 const express = require('express');
 const router = express.Router();
 
-// 상품 목록
-router.get('/', (req, res) => {
-  res.render('products'); // products.ejs로 렌더링
-});
+// 더미 상품 데이터
+const products = [
+    { id: 1, name: '나는야 감자빵', desc: '국내산 감자, 앙금, 찹쌀', price: '₩3,000' },
+    { id: 2, name: '내고향 고구마빵', desc: '국내산 고구마, 앙금, 찹쌀', price: '₩3,500' },
+    { id: 3, name: '내고향 밤빵', desc: '국내산 밤, 앙금, 찹쌀', price: '₩3,500' },
+    { id: 4, name: '내고향 옥수수빵', desc: '강원도 미백 찰옥수수', price: '₩4,000' },
+    { id: 5, name: '내고향 마늘빵', desc: '국내산 마늘, 치즈, 찹쌀', price: '₩5,000' },
+    { id: 6, name: '내고향 대파빵', desc: '국내산 대파, 치즈, 찹쌀', price: '₩5,000' },
+  ];
 
-// 상품 상세 페이지
-router.get('/:id', (req, res) => {
-  const productId = req.params.id;
-  // 실제로는 DB에서 productId로 상품 정보 가져오기
-  res.render('detail', { id: productId });
-});
+// 전체 상품 페이지
+    router.get('/', (req, res) => {
+    res.render('products', { products });
+  });
+  
+  // 상품 상세보기
+  router.get('/:id', (req, res) => {
+    const productId = parseInt(req.params.id);
+    const product = products.find(p => p.id === productId);
+  
+    if (!product) {
+      return res.status(404).send('상품을 찾을 수 없습니다.');
+    }
+  
+    res.render('products_detail', { product });
+  });
+  
 
 module.exports = router;
