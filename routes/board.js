f377809c05ca73686c44c1ff2339d6191426fc5e
soyuniii <pe1229@naver.com>
2025-06-07 23:24:30 +0900

    문의하기 username 반영

--- routes/board.js
+++ routes/board.js
@@ -40,7 +40,7 @@ router.get('/new', (req, res) => {
         parentId: null ,
         parentTitle: null, 
         session: req.session, 
-        user: req.session.username || null
+        username: req.session.username || null
     });
 });
 
